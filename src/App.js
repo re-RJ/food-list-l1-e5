f3a4b928937edac20d1d5ea777860a0b11950550
rj_m <rjmarte@sbcglobal.net>
2026-06-17 15:55:15 -0700

    Adding state to allow UI to update with input and button click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import NewFoodInput from "./NewFoodInput";
 import "./styles.css";
 
 export default function App() {
+  const [foodsToTry, setFoodsToTry] = useState([]);
+  
+  const addFoodItem = (food) => {
+    setFoodsToTry([...foodsToTry, food])
+  }
+  
   return (
     <div className="App">
       <h1>New Foods to Try</h1>
-      <NewFoodInput />
-      <ul className="food-list"></ul>
+      <NewFoodInput addFoodItem={addFoodItem}/>
+      <ul className="food-list">
+        {foodsToTry.map((food, index) => (
+          <li key={index}>{food}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/NewFoodInput.js
+++ src/NewFoodInput.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NewFoodInput = () => {
+const NewFoodInput = (props) => {
+  const [food, setFood] = useState("");
+
+  const updateFood = (e) => {
+    setFood(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addFoodItem(food)
+    setFood("");
+  };
+  
   return (
-    <form className="food-form">
-      <input type="text" aria-label="Add a food to the list" />
+    <form className="food-form" onSubmit={handleSubmit}>
+      <input type="text" aria-label="Add a food to the list" value={food} onChange={updateFood} />
       <button>Add to the list!</button>
     </form>
   );
